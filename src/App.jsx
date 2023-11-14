4d18c2c0d8c86f0f96cabd39358c15ab2e7d5ec6
NelsonBotero1518 <nelsonkennedy2004@gmail.com>
2023-11-14 15:42:45 -0500

    correccion router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Menu from "./components/Menu";
-import { Navigate, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./components/Home";
 import SobreMi from "./components/SobreMi";
 import Proyectos from "./components/Proyectos";
